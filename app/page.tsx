ffe9027de5c64862d35ee2167022029ec4c1209a
Max <glukhov_maxim@mail.ru>
2026-03-06 17:09:33 +0700

    Add LikeButton component with like functionality and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { JSX } from 'react';
-import { Card } from './components';
+'use client';
 
-const data = [
+import { JSX, useState } from 'react';
+import { Card } from './components';
+import { LikeButton } from './components/LikeButton/LikeButton';
+
+const initialData = [
 	{
 		id: "1",
 		title: "Как работать с CSS Grid",
@@ -10,20 +13,68 @@ const data = [
 		category: "Front-end",
 		publishedAt: "1 месяц назад",
 		readingTime: "3 минуты",
-		likesCount: 4,
+		likesCount: 0,
 		userLiked: false,	
 	}
 ];
 
 export default function Home(): JSX.Element {
+	const [posts, setPosts] = useState(initialData);
+	
+	const handleLikeToggle = async (postId: number | string, newState: boolean): Promise<void> => {
+		setPosts((prevPosts) => prevPosts.map((post) => {
+			if (post.id === postId){
+				return {
+					...post,
+					userLiked: newState,
+					likesCount: newState ? post.likesCount + 1 : post.likesCount - 1,
+				};
+			}
+			return post;
+		})
+	);
+
+	try {
+		const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+			method: 'PATCH',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify({ userLiked: newState }),
+		});
+
+		if (!response.ok) throw new Error('PATCH failed');
+	} catch (error) {
+		setPosts((prevPosts) => 
+			prevPosts.map((post) => {
+				if (post.id === postId) {
+					return {
+						...post,
+						userLiked: !newState,
+						likesCount: !newState ? post.likesCount + 1 : post.likesCount - 1
+					};
+				}
+				return post;
+			})
+		);
+		console.error(error);
+		throw error;
+		}
+	};
+		
+		
+
 	return (
 		<>
-			{data.map((card) => (
+			{posts.map((card) => (
 				<Card
 					key={card.id}
 					{...card}
 				/>
 			))}
+			<LikeButton
+				postId={posts[0].id}
+				initialLiked={posts[0].userLiked}	
+				onLikeToggle={handleLikeToggle}
+			/>
 		</>
 	);
 }
